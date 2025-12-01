121f2900a6a90b9142ba6a89ba8fd5c0f696b6be
Gavin Walker <gavinwalker@Gavins-iMac.local>
2025-12-01 23:04:01 +0000

    Fix db_session in prettify endpoint

--- app.py
+++ app.py
@@ -1821,7 +1821,8 @@ async def prettify_item(
         raise HTTPException(status_code=401, detail="Not authenticated")
     
     token = authorization.split(' ')[1]
-    user = get_current_user(db_session(), token)
+    db = next(get_db())
+    user = get_current_user(db, token)
     if not user:
         raise HTTPException(status_code=401, detail="Invalid token")
     
